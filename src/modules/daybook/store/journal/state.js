cf140a07fb1e7476971748eedc3d41848205b109
MaryuriAndreaRamirez <maryuriandrea14@gmail.com>
2022-01-07 10:14:00 -0500

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,25 @@
+
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        { 
+            id: new Date().getTime(), //12371298317892
+            date: new Date().toDateString, // sat 23, julio
+            text: 'Commodo sit elit elit occaecat occaecat exercitation. Cupidatat proident ea cupidatat ex. Do laboris ullamco eu pariatur proident anim id. Cillum dolor sit commodo anim eu commodo pariatur aliquip esse sunt nulla voluptate consectetur',
+            picture: null, // https://
+        },
+        { 
+            id: new Date().getTime() + 1000, //12371298317892
+            date: new Date().toDateString, // sat 23, julio
+            text: 'Qui labore qui occaecat velit. Fugiat enim cupidatat proident amet in eiusmod do nostrud dolore sint eiusmod reprehenderit. Aliquip enim proident non incididunt culpa. Ea ipsum deserunt adipisicing consectetur. Amet adipisicing aliquip cillum commodo in nulla minim reprehenderit. Sint labore adipisicing ut magna est incididunt sint mollit irure.',
+            picture: null, // https://
+        },
+        { 
+            id: new Date().getTime() + 2000, //12371298317892
+            date: new Date().toDateString, // sat 23, julio
+            text: 'Non ut minim reprehenderit enim cillum ipsum occaecat duis amet in ea fugiat qui nostrud. Id minim laborum aute tempor dolore occaecat exercitation enim elit exercitation. Dolor mollit nulla et veniam pariatur veniam incididunt eiusmod quis anim culpa. Do voluptate duis do laborum tempor elit id nisi adipisicing exercitation.',
+            picture: null, // https://
+        },
+    ]
+})
---
+++ src/modules/daybook/views/EntryView.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="entry-title d-flex justify-content-between p-2">
+        <div>
+            <span class="text-success fs-3 fw-bold">06</span>
+            <span class="mx-1 fs-3">Enero</span>
+            <span class="mx-2 fs-4 fw-light">2022, jueves</span>
+        </div>
+
+        <div>
+            <button class="btn btn-danger mx-2">
+                Borrar
+                <i class="fa fa-trash-alt"></i>
+            </button>
+
+            <button class="btn btn-primary">
+                subir foto
+                <i class="fa fa-upload"></i>
+            </button>
+        </div>
+    </div>
+
+    <hr>
+    <div class="d-flex flex-column px-3 h-75">
+        <!-- <textarea name="" id="" cols="30" rows="10"></textarea> -->
+        <textarea
+            placeholder="¿Qué sucedió hoy?"
+        ></textarea>
+    </div>
+
+    <Fab 
+        icon="fa-save"
+    />
+
+    <img 
+        src="https://www.robertlandscapes.com/wp-content/uploads/2014/11/landscape-322100_1280.jpg" alt="entry-picture"
+        class="img-thubnail">
+
+</template>
+
+<script>
+import { defineAsyncComponent } from '@vue/runtime-core'
+
+export default {
+    components: {
+        Fab: defineAsyncComponent(() => import('../components/Fab.vue'))
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    textarea {
+        font-size: 20px;
+        border: none;
+        height: 100%;
+
+        &:focus {
+            outline: none;
+        }
+    }
+
+    img {
+        bottom: 150px;
+        box-shadow: 0pc 5 px 10px rgba($color: #000000, $alpha: 0.2);
+        position: fixed;
+        right: 20px;
+        width: 200px;
+    }
+</style>
